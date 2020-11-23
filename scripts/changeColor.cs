ec81e6003c06d1f5afa81a6c173b0037b58a7fcb
Esther Jorge <alu0101102498@ull.edu.es>
2020-11-23 21:30:27 +0000

    Scripts y apk

---
+++ scripts/changeColor.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class changeColor : MonoBehaviour
+{
+    private Renderer rd;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rd = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Red() {
+        rd.material.color = Color.red;
+    }
+
+    public void Blue() {
+        rd.material.color = Color.blue;
+    }   
+    
+    public void Black() {
+        rd.material.color = Color.black;
+    }
+}
---
+++ scripts/pickUp.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pickUp : MonoBehaviour
+{
+    public int countCubes;
+    // Start is called before the first frame update
+    void Start()
+    {
+        countCubes = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void recolect() {
+        countCubes++;
+        Destroy(gameObject);
+    }
+}
